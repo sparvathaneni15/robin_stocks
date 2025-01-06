e2e3d65d3528a9c88674502d1cc4b25bcdfcb4e3
Samanyu Parvathaneni <psamanyu@gmail.com>
2025-01-06 13:24:55 -0800

    Truncated floats to two decimals places

--- buy_current_holdings_rira.py
+++ buy_current_holdings_rira.py
@@ -15,13 +15,17 @@ account_number = os.environ["rira_account_number"]
 
 my_stocks = r.build_holdings(account_number=account_number)
 
-print("Buying Power: " + r.profiles.load_account_profile(account_number=account_number, info='buying_power'))
+print("Buying Power: " + "{:.2f}".format(float(r.profiles.load_account_profile(account_number=account_number, info='buying_power'))))
 
 for item, data in my_stocks.items():
     current_price = float(data['price'])
     average_buy_price = float(data['average_buy_price'])
     name = item
 
-    dollar_amount = float(input(f"Enter the dollar amount you want to invest in {name} (Current Price: {current_price}): $"))
+    # Display current price with 2 decimal points
+    dollar_amount = float(input(f"Enter the dollar amount you want to invest in {name} (Current Price: {current_price:.2f}): $"))
+    
     order = r.order_buy_fractional_by_price(name, dollar_amount, account_number=account_number)
-    print("Purchased " + order['quantity'] + " shares of " + name + " at " + order['price'])
+    
+    # Display purchased quantity and price with 2 decimal points
+    print("Purchased {:.2f} shares of {} at ${:.2f}".format(float(order['quantity']), name, float(order['price'])))
